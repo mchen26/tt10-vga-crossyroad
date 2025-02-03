79e1c58e39b3493fbe8d95fbd0c8dab80ea67e07
Jovan Koledin <jovank@uw.edu>
2025-02-02 18:35:19 -0800

    Bug

--- src/scroll_h_follower.v
+++ src/scroll_h_follower.v
@@ -1,28 +1,27 @@
-module scroll_v_follower (
-    output reg [9:0] y_pos, // Counter for scrolling down
-    input wire [9:0] start_posy,
-    input wire move, // scroll_v tells when to move
+module scroll_h_follower (
+    output reg [9:0] h_pos, // Counter for scrolling down
     input wire reset,
+    input wire [9:0] start_posx,
+    input wire move,
     input wire clk
 );
 
     // Parameters
     localparam move_amt = 2;           // Move amount per 40ms
-    localparam SCREEN_HEIGHT = 480;    // Screen height
-  
+    localparam SCREEN_WIDTH = 640;    // Screen height
+
     // Obstacle Movement Logic
     always @(posedge clk) begin
         if (reset) begin
-            y_pos <= start_posy;        // Reset position to top
+            h_pos <= start_posx;                // Reset position to top
         end else begin
-          if (move) begin
-              if ((y_pos + move_amt) >= SCREEN_HEIGHT) begin
-                  y_pos <= 0;
-              end else begin
-                  y_pos <= y_pos + move_amt;
-              end    
-          end
+            if (move) begin
+                if ((h_pos + move_amt) >= SCREEN_WIDTH) begin
+                    h_pos <= 0;
+                end else begin
+                    h_pos <= h_pos + move_amt;
+                end 
+            end
         end
     end
-
 endmodule
--- src/scroll_v_follower.v
+++ src/scroll_v_follower.v
@@ -1,8 +1,7 @@
-module scroll_v (
+module scroll_v_follower (
     output reg [9:0] y_pos, // Counter for scrolling down
-    output reg [7:0] score,
     input wire [9:0] start_posy,
-    input wire move_btn,
+    input wire move, // scroll_v tells when to move
     input wire reset,
     input wire clk
 );
@@ -10,48 +9,19 @@ module scroll_v (
     // Parameters
     localparam move_amt = 2;           // Move amount per 40ms
     localparam SCREEN_HEIGHT = 480;    // Screen height
-    localparam SPEED = 100000;          // 40ms at 25MHz clock
-    localparam SCORE_SPEED = 100;       // 1s score update
   
-    // Internal Registers
-    reg [17:0] ctr;                    // Counter for timing
-    reg [7:0] score_ctr;
-    reg move_active;                   // Track button press state
-
     // Obstacle Movement Logic
     always @(posedge clk) begin
         if (reset) begin
             y_pos <= start_posy;        // Reset position to top
-            ctr <= 0;                  // Reset counter
-            move_active <= 0;          // Deactivate movement
-            score_ctr <= 0;
-            score <= 0;
         end else begin
-            if (move_btn) begin
-                move_active <= 1;      // Activate movement
-            end else begin
-                move_active <= 0;      // Deactivate movement
-            end
-
-            // Counter for speed control
-            if (move_active) begin
-                ctr <= ctr + 1;
-                if (ctr >= SPEED) begin
-                    ctr <= 0;
-                    score_ctr <= score_ctr + 1; 
-                    if ((y_pos + move_amt) >= SCREEN_HEIGHT) begin
-                        y_pos <= 0;
-                    end else begin
-                        y_pos <= y_pos + move_amt;
-                    end    
-                end
-                if (score_ctr == SCORE_SPEED) begin
-                    score_ctr <= 0;
-                    score <= score + 1;
-                end
-            end else begin
-                ctr <= 0; // Reset counter when not moving
-            end
+          if (move) begin
+              if ((y_pos + move_amt) >= SCREEN_HEIGHT) begin
+                  y_pos <= 0;
+              end else begin
+                  y_pos <= y_pos + move_amt;
+              end    
+          end
         end
     end
 
